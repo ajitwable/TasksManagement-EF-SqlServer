831c7ce878aab438c9880f47160059495a1ae5f9
Dell <Dell@DESKTOP-MDQMFGT>
2025-01-31 12:28:29 +0530

    Add project files.

---
+++ TasksManagement/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TasksManagement.Entities;
+
+namespace TasksManagement.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tasks> task { get; set; }
+    }
+}
---
+++ TasksManagement/Entities/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TasksManagement.Entities
+{
+    public class Tasks
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
